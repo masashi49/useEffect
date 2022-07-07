1895d719153db6a04c39973a0bca49864f542731
yamadamasashi <shisama94@gmail.com>
2022-07-07 09:41:01 +0900

    ジェネリクス練習

--- test-useeffect/src/components/common/form/element/Input/Input.tsx
+++ test-useeffect/src/components/common/form/element/Input/Input.tsx
@@ -6,6 +6,15 @@ export const Input = forwardRef<
   HTMLInputElement,
   ComponentPropsWithoutRef<"input">
 >(({ className, ...props }, ref) => {
+
+
+  function copy<T extends { name: string }, U extends keyof T>(value: T, key: U) {
+    return value[key]
+  }
+  console.log(copy({ name: "hello", hello: 49 }, 'name'))
+
+  type K = { name: string; age: number }
+
   return (
     <input
       type="text"
@@ -14,3 +23,25 @@ export const Input = forwardRef<
     />
   );
 });
+
+class base<T> {
+  private data: T[] = []
+
+  add(item: T) {
+    this.data.push(item)
+  }
+  remove(item: T) {
+    this.data.splice(this.data.indexOf(item), 1)
+  }
+  get() {
+    return this.data
+  }
+}
+
+const hoge = new base<number>()
+
+hoge.add(1)
+hoge.add(2)
+hoge.add(3)
+hoge.remove(2)
+console.log(hoge.get())
