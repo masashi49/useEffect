fde8498784170fede0de373885f0cc774a4fcf77
yamadamasashi <shisama94@gmail.com>
2022-07-11 09:49:05 +0900

    HtmlTextAreaelementについて調べる

--- test-useeffect/src/components/common/form/element/TextArea/TextArea.tsx
+++ test-useeffect/src/components/common/form/element/TextArea/TextArea.tsx
@@ -1,7 +1,7 @@
-import { FC } from 'react';
+import { ComponentPropsWithoutRef, forwardRef } from 'react';
 
-type Props = JSX.IntrinsicElements['textarea']
-
-export const TextArea: FC<Props> = ({ ...props }) => {
-  return <textarea {...props} />
-}
+export const TextArea = forwardRef<
+  HTMLTextAreaElement, ComponentPropsWithoutRef<'textarea'>
+>(({ className, ...props }, ref) => {
+  return <textarea {...props} ref={ref} />
+})
